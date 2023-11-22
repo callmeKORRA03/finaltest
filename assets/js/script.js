c0d8c2d412515cf71af9564a4fe1ab069ea86ade
fave <iamkorra03@gmail.com>
2023-11-22 14:32:59 +0100

    final Work

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function darkmode() {
+    var element = document.body;
+    var heeehe = document.getElementById('header');
+    element.classList.toggle('dark-mode');
+    element.getEle
+    element.style.transition = '1s'
+
+    return element  
+}
